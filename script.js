3a088b773a6ce0c1acf5b696803360eb29949a7f
Maxime <maxime@macbook-air-de-oceane-2.local>
2021-10-06 08:43:15 +0000

    Formatage

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-window.addEventListener('load', () => {
-    console.log("Canva chargé!");
+window.addEventListener("load", () => {
+  console.log("Canva chargé!");
 
-    const canvas = document.querySelector('#canvas');
-    const ctx = canvas.getContext("2d");
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
 
-    //resizing
+  //resizing
 
-    //variables
-    let painting = false;
+  //variables
+  let painting = false;
 
-    //methods
-    function startPosition(e) {
-        painting = true;
+  //methods
+  function startPosition(e) {
+    painting = true;
 
-        //when click only
-        draw(e)
-    }
+    //when click only
+    draw(e);
+  }
 
-    function finishedPosition() {
-        painting = false;
+  function finishedPosition() {
+    painting = false;
 
-        //after start from new position
-        ctx.beginPath();
+    //after start from new position
+    ctx.beginPath();
 
-        // POST 
-        let apiData = "";
-    }
+    // POST
+    let apiData = "";
+  }
 
-    function draw(e) {
-        if (!painting) return;
+  function draw(e) {
+    if (!painting) return;
 
-        //style
-        ctx.lineWidth = document.getElementById('pencilWidth').value;
-        ctx.lineCap = "round";
+    //style
+    ctx.lineWidth = document.getElementById("pencilWidth").value;
+    ctx.lineCap = "round";
 
-        ctx.lineTo(e.clientX, e.clientY);
-        ctx.stroke();
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+  }
 
-    }
-
-    //listener
-    canvas.addEventListener('mousedown', startPosition);
-    canvas.addEventListener('mouseup', finishedPosition);
-    canvas.addEventListener('mousemove', draw);
-})
+  //listener
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishedPosition);
+  canvas.addEventListener("mousemove", draw);
+});
