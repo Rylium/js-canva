f576f0e6ca7d26bc938b0c1ce1cebf458f870d89
Maxime <maxime@macbook-air-de-oceane-2.local>
2021-10-06 12:53:34 +0000

    Getdraw

--- script.js
+++ script.js
@@ -1,71 +1,69 @@
 window.addEventListener("load", () => {
-    console.log("Canva chargé!");
+  console.log("Canva chargé!");
 
-    const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContext("2d");
-    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
+  var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-    let colorPicker = document
-        .getElementById("colorPicker")
-        .addEventListener("change", onChangeColor);
+  let colorPicker = document
+    .getElementById("colorPicker")
+    .addEventListener("change", onChangeColor);
 
-    let painting = false;
-    let draws, path, lastId;
+  let painting = false;
+  let draws, path, lastId;
 
-    // Récupération des données de l'Api
-    function getDraw(lastId) {
-        $.ajax("https://api.draw.codecolliders.dev/paths").done(function (data) {
-            // console.log(data);
-            draws = data;
-            for (const draw of draws) {
-                lastId = draw.id;
-                path = draw.path;
-
-                path.forEach(element => console.log(element[1]));
-                // Pour chaqué element, redéfinir chaque sous-élément sous "element" et choisir d'afficher sa première propriété (x)
-                path.forEach(element => console.log(element[2])); // (y)
-            }
-        });
-    }
-    getDraw();
-
-
-    function onChangeColor() {
-        ctx.strokeStyle = this.value;
-    }
-
-    //methods
-    function startPosition(e) {
-        painting = true;
-        console.log(imgData)
-        //when click only
-        draw(e);
-    }
-
-    function finishedPosition() {
-        painting = false;
-
-        //after start from new position
+  // Récupération des données de l'Api
+  function getDraw() {
+    $.ajax("https://api.draw.codecolliders.dev/paths").done(function (data) {
+      for (const draw of data) {
         ctx.beginPath();
-
-        // POST
-        let apiData = "";
-    }
-
-
-    function draw(e) {
-        if (!painting) return;
-
-
-        //style
-        ctx.lineWidth = document.getElementById("pencilWidth").value;
-        ctx.lineCap = "round";
-        ctx.lineTo(e.clientX, e.clientY);
+        ctx.strokeStyle = draw["strokeColor"];
+        ctx.lineWidth = draw["lineWidth"];
+        ctx.moveTo(draw.path[0], draw.path[1]);
+        for (const path of draw.path) {
+          ctx.lineTo(path[0], path[1]);
+        }
         ctx.stroke();
-    }
+      }
+    });
+  }
 
-    //listener
-    canvas.addEventListener("mousedown", startPosition);
-    canvas.addEventListener("mouseup", finishedPosition);
-    canvas.addEventListener("mousemove", draw);
+  getDraw();
+
+  function onChangeColor() {
+    ctx.strokeStyle = this.value;
+  }
+
+  //methods
+  function startPosition(e) {
+    painting = true;
+
+    //when click only
+    draw(e);
+  }
+
+  function finishedPosition() {
+    painting = false;
+
+    //after start from new position
+    ctx.beginPath();
+
+    // POST
+    let apiData = "";
+  }
+
+  function draw(e) {
+    if (!painting) return;
+
+    //style
+    ctx.lineWidth = document.getElementById("pencilWidth").value;
+    ctx.lineCap = "round";
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+  }
+
+  //listener
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishedPosition);
+  canvas.addEventListener("mousemove", draw);
 });
