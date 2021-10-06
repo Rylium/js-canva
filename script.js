4ad27201f7ffdb7fe655dcbccecfc7e9c6064af3
Maxime <maxime@macbook-air-de-oceane-2.local>
2021-10-06 13:17:33 +0000

    Commentaires

--- script.js
+++ script.js
@@ -1,84 +1,85 @@
 window.addEventListener("load", () => {
-  console.log("Canva chargé!");
 
-  const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext("2d");
-  var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    // Déclaration des variables
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let colorPicker = document
+        .getElementById("colorPicker")
+        .addEventListener("change", onChangeColor);
+    let painting = false;
+    let draws, path, lastId;
 
-  let colorPicker = document
-    .getElementById("colorPicker")
-    .addEventListener("change", onChangeColor);
 
-  let painting = false;
-  let draws, path, lastId;
+    // Récupération des données de l'Api
+    function getDraw() {
+        $.ajax("https://api.draw.codecolliders.dev/paths").done(function(data) {
+            for (const draw of data) {
+                ctx.beginPath();
+                ctx.strokeStyle = draw["strokeColor"];
+                ctx.lineWidth = draw["lineWidth"];
+                ctx.moveTo(draw.path[0], draw.path[1]);
+                for (const path of draw.path) {
+                    ctx.lineTo(path[0], path[1]);
+                }
+                ctx.stroke();
+            }
+        });
+    }
 
-  // Récupération des données de l'Api
-  function getDraw() {
-    $.ajax("https://api.draw.codecolliders.dev/paths").done(function (data) {
-      for (const draw of data) {
+    // Ajout de dessins a l'API
+    function sendDraw() {
+        $.ajax({
+            url: "https://api.draw.codecolliders.dev/paths/add",
+            headers: {
+                "x-api-key": token,
+                "Content-Type": "application/json",
+            },
+            method: "post",
+            data: JSON.stringify({}),
+            processData: false,
+        }).done();
+    }
+
+    // Exécution de la fonction + Rafraîchissement des données 
+    getDraw();
+    setInterval(getDraw, 2000);
+
+    // Fonction au changement de couleur
+    function onChangeColor() {
+        ctx.strokeStyle = this.value;
+    }
+
+    //methods
+    function startPosition(e) {
+        painting = true;
+
+        //when click only
+        draw(e);
+    }
+
+    function finishedPosition() {
+        painting = false;
+
+        //after start from new position
         ctx.beginPath();
-        ctx.strokeStyle = draw["strokeColor"];
-        ctx.lineWidth = draw["lineWidth"];
-        ctx.moveTo(draw.path[0], draw.path[1]);
-        for (const path of draw.path) {
-          ctx.lineTo(path[0], path[1]);
-        }
+
+        // POST
+        let apiData = "";
+    }
+
+    function draw(e) {
+        if (!painting) return;
+
+        //style
+        ctx.lineWidth = document.getElementById("pencilWidth").value;
+        ctx.lineCap = "round";
+        ctx.lineTo(e.clientX, e.clientY);
         ctx.stroke();
-      }
-    });
-  }
+    }
 
-  //Ajout de dessins a l'API
-  function sendDraw() {
-    $.ajax({
-      url: "https://api.draw.codecolliders.dev/paths/add",
-      headers: {
-        "x-api-key": token,
-        "Content-Type": "application/json",
-      },
-      method: "post",
-      data: JSON.stringify({
-        image_id: idChat,
-      }),
-      processData: false,
-    }).done(displayFavourite);
-  }
-  getDraw();
-
-  function onChangeColor() {
-    ctx.strokeStyle = this.value;
-  }
-
-  //methods
-  function startPosition(e) {
-    painting = true;
-
-    //when click only
-    draw(e);
-  }
-
-  function finishedPosition() {
-    painting = false;
-
-    //after start from new position
-    ctx.beginPath();
-
-    // POST
-    let apiData = "";
-  }
-
-  function draw(e) {
-    if (!painting) return;
-
-    //style
-    ctx.lineWidth = document.getElementById("pencilWidth").value;
-    ctx.lineCap = "round";
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.stroke();
-  }
-
-  //listener
-  canvas.addEventListener("mousedown", startPosition);
-  canvas.addEventListener("mouseup", finishedPosition);
-  canvas.addEventListener("mousemove", draw);
+    // Ajout d'event
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
 });
